d570f4f9967f08b29aca30e77d1991a2f728a509
amanpujara <amanpujara333.in>
2026-03-15 17:10:18 +0530

    Add B06_Operators.java demonstrating arithmetic, relational, and logical operators

---
+++ 01-basics/B06_Operators.java
@@ -0,0 +1,29 @@
+// File: B06_Operators.java
+// Class Name: B06_Operators
+// Purpose: Demonstrates different types of operators in Java
+
+public class B06_Operators {
+    public static void main(String[] args) {
+
+        int a = 10, b = 5;
+
+        // Arithmetic Operators
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Modulus: " + (a % b));
+
+        // Relational Operators
+        System.out.println("a == b: " + (a == b));
+        System.out.println("a != b: " + (a != b));
+        System.out.println("a > b: " + (a > b));
+        System.out.println("a < b: " + (a < b));
+
+        // Logical Operators
+        boolean x = true, y = false;
+        System.out.println("x && y: " + (x && y));
+        System.out.println("x || y: " + (x || y));
+        System.out.println("!x: " + (!x));
+    }
+}
