da98b9e443c61b5efd2fbad6ca9c4f1a3874fd7a
amanpujara <amanpujara333.in>
2026-03-16 00:40:17 +0530

    Add C05_DoWhileLoop.java demonstrating do-while loop

---
+++ 03-patterns/C05_DoWhileLoop.java
@@ -0,0 +1,14 @@
+// File: C05_DoWhileLoop.java
+// Class Name: C05_DoWhileLoop
+// Purpose: Demonstrates the do-while loop in Java
+
+public class C05_DoWhileLoop {
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println("Printing numbers from 1 to 5 using do-while loop:");
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= 5);
+    }
+}
