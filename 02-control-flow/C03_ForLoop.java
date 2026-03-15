911415b1feff1efe14b15f92bef976922a360d54
amanpujara <amanpujara333.in>
2026-03-15 22:52:44 +0530

    Add C03_ForLoop.java demonstrating for loop

---
+++ 02-control-flow/C03_ForLoop.java
@@ -0,0 +1,12 @@
+// File: C03_ForLoop.java
+// Class Name: C03_ForLoop
+// Purpose: Demonstrates the for loop in Java
+
+public class C03_ForLoop {
+    public static void main(String[] args) {
+        System.out.println("Printing numbers from 1 to 5 using for loop:");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
