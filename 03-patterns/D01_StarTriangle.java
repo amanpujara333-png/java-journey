d88fc5bab8040ed75a1e192c33a12ca64ac45262
amanpujara <amanpujara333.in>
2026-03-15 23:29:47 +0530

    Add D01_StarTriangle.java demonstrating simple star triangle pattern

---
+++ 03-patterns/D01_StarTriangle.java
@@ -0,0 +1,14 @@
+// File: D01_StarTriangle.java
+// Purpose: Print a simple star triangle pattern
+
+public class D01_StarTriangle {
+    public static void main(String[] args) {
+        int rows = 5;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
