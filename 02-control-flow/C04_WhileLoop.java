eeb20c61671ccfa2cae73feabd5b5f076c8446b7
amanpujara <amanpujara333.in>
2026-03-15 22:59:15 +0530

    Add C04_WhileLoop.java demonstrating while loop

---
+++ 02-control-flow/C04_WhileLoop.java
@@ -0,0 +1,14 @@
+// File: C04_WhileLoop.java
+// Class Name: C04_WhileLoop
+// Purpose: Demonstrates the while loop in Java
+
+public class C04_WhileLoop {
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println("Printing numbers from 1 to 5 using while loop:");
+        while (i <= 5) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
