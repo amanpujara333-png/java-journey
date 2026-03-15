aa7b7a8ecdc02c2760603ac3c6dc4987e6ece17b
amanpujara <amanpujara333.in>
2026-03-15 17:17:38 +0530

    Add B07_SwapNumbers.java demonstrating swapping two numbers

---
+++ 01-basics/B07_SwapNumbers.java
@@ -0,0 +1,20 @@
+// File: B07_SwapNumbers.java
+// Class Name: B07_SwapNumbers
+// Purpose: Demonstrates swapping two numbers in Java
+
+public class B07_SwapNumbers {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Before swap: a = " + a + ", b = " + b);
+
+        // Swap using a temporary variable
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("After swap: a = " + a + ", b = " + b);
+    }
+}
