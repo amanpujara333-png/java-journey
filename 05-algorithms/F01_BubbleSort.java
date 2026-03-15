cd7df6d737ade6cbf192c917001fcacef55b8e16
amanpujara <amanpujara333.in>
2026-03-16 01:06:23 +0530

    Add F01_BubbleSort.java demonstrating bubble sort

---
+++ 05-algorithms/F01_BubbleSort.java
@@ -0,0 +1,22 @@
+// File: F01_BubbleSort.java
+// Purpose: Demonstrates bubble sort algorithm
+
+public class F01_BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 1, 5, 6};
+        int n = arr.length;
+
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+
+        System.out.print("Sorted array: ");
+        for (int num : arr) System.out.print(num + " ");
+    }
+}
