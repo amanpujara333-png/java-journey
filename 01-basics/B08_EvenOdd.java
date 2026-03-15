da176d766230bbbb3bc80a463a3e3c3c0e78575e
amanpujara <amanpujara333.in>
2026-03-15 17:19:58 +0530

    Add B08_EvenOdd.java to check even or odd numbers

---
+++ 01-basics/B08_EvenOdd.java
@@ -0,0 +1,16 @@
+// File: B08_EvenOdd.java
+// Class Name: B08_EvenOdd
+// Purpose: Checks if a number is even or odd
+
+public class B08_EvenOdd {
+    public static void main(String[] args) {
+
+        int number = 15;
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+    }
+}
